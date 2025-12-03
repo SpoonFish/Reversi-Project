8cd05f77a8a358a113e9d163da5ac8e280e62c13
SpoonFish <89307474+SpoonFish@users.noreply.github.com>
2025-12-03 03:28:48 +0000

    Added comments

--- Stage1/components.py
+++ Stage1/components.py
@@ -9,6 +9,7 @@ def initialise_board(size=8):
         list[list[str]]: 2D list of cells representing the board game. Each cell will be either "None ", "Dark ", or "Light".
     """
 
+    # Check the value of the 'size' parameter is valid and appropriate for being the width and height of the board
     if not isinstance(size, int):
         raise TypeError("Board size must be an integer.")
     if size % 2 != 0:
@@ -20,9 +21,11 @@ def initialise_board(size=8):
     
     board = []
 
+    # Fill the board with rows of the string "None "
     for _ in range(size):
         board.append(["None " for _ in range(size)])
 
+    # Set the middle 4 squares of the board as an alternating pattern of "Dark " and "Light"
     board[size//2][size//2] = "Dark "
     board[size//2-1][size//2-1] = "Dark "
     board[size//2-1][size//2] = "Light"
@@ -43,37 +46,60 @@ def legal_move(colour,coord,board):
         bool: Whether the move is legal or not.
     """
 
+    # Reduce values in the coordinate tuple by 1 so that they translate to zero-based indices of the board lists
     x = coord[0] - 1
     y = coord[1] - 1
-    target_space = board[y][x]
+
+    # Get the state of the cell at the desired position for the move
+    target_cell = board[y][x]
+
     size = len(board)
-    if target_space != "None ":
+
+    # The move cannot be legal if the cell is already occupied by any counter
+    if target_cell != "None ":
         return False
     else:
+        # Get the opposite colour to the colour of the player making the move
         opposite_colour = "Dark " if colour == "Light" else "Light"
+
+        # Stores a set of directions vectors which will act as displacements from the position of the move to check if the states of the surrounding cells allow the move to be legal
         valid_directions = [(-1,-1),(0,-1),(1,-1),(-1,0),(1,0),(-1,1),(0,1),(1,1)]
+
         directions_to_remove = []
+
+        # Search the surrounding cells outwards in direct straight lines from the specified move position
         for step in range(1,size):
+
+            # Remove all the direction vectors that are confirmed to lead to invalid moves from the previous iteration before checking the next surrounding cells
             for d in directions_to_remove:
                 valid_directions.remove(d)
 
+            # No directions of search can lead to a possible legal move from the specified position so exit the search
             if len(valid_directions) == 0:
                 break
 
+            # Reset the list so that directions are not removed twice
             directions_to_remove.clear()
 
             for direction in valid_directions:
+                # Get the change in position from x and y to calculate the position of the cell being checked
                 dx = direction[0]*step
                 dy = direction[1]*step
 
+                # Do not check the cell if the position to be searched is outside the board
                 if 0 > x+dx or x+dx >= size or 0 > y+dy or y+dy >= size: 
                     continue
 
-                check_space = board[y + dy][x + dx]
+                # The status of the cell being checked
+                check_cell = board[y + dy][x + dx]
 
-                if step > 1 and check_space == colour:
+                # There is a counter of the same colour in a straight line from the desired position that has at least 1 counter of the opposite colour between them, so the move is legal
+                if step > 1 and check_cell == colour:
                     return True
-                if check_space != opposite_colour:
+                
+                # This checks if an empty cell is reached before reaching a cell of the current player's colour, if there is then this direction of search will not lead to any legal moves
+                # It also checks if the cell adjacent to the desired position in the current direction is the same colour as the current player's colour. This would lead to no counters of the opposite colour ending up between the desired position and the next counter of the same colour in a line so the current direction would lead to no legal moves
+                if check_cell != opposite_colour:
                     directions_to_remove.append(direction)
         return False
 
@@ -87,18 +113,31 @@ def print_board(board):
         board (list[list[str]]): The board containing the current status of each cell in the game.
     """
     size = len(board)
+
+    # Padding for the top left corner of the representation
     print(end="   ")
+
+    # Print the column numbers celld so that they are directly above the column they are associated with
     for i in range(size):
+        # Numbers with 2 digits need less padding
         if i+1 < 10:
             print(i+1, end="     ")
         else:
             print(i+1, end="    ")
+
+    # Start the printing of rows on a new line
     print()
+
+    # Print the row number along with the status of each cell in the row
     for i, row in enumerate(board):
+        # Numbers with 2 digits need less padding
         if i+1 < 10:
             print(i+1, end="  ")
         else:
             print(i+1, end=" ")
-        for space in row:
-            print(space, end=" ")
+
+        for cell in row:
+            print(cell, end=" ")
+
+        # Print a new line so rows are not all printed on the same line
         print()
--- Stage1/game_engine.py
+++ Stage1/game_engine.py
@@ -10,17 +10,30 @@ def cli_input_coords():
         tuple(int,int): The x and y position of the players desired move.
     """
     
+    # Keep asking the player for a move position until a valid move is given
     while True:
         x = input("Enter x coordinate of move: ")
         y = input("Enter y coordinate of move: ")
+
+        # Checks inputted coordinates are whole positive numbers
         if not x.isdecimal() or not y.isdecimal():
             print("Coordinates must both be whole numbers. Try again")
             continue
+
+        # Disallow numbers longer than necessary to avoid extremely long digit numbers that cause str to int conversion to error (>4300 digit numbers)
+        if len(x) > 2 or len(y) > 2:
+            print("Inputted numbers are too long. Try again")
+            continue
+
+        # They are safe to convert into integers now
         x = int(x)
         y = int(y)
+
+        # Check the coordinates are on the board
         if x < 1 or y < 1 or x > BOARD_SIZE or y > BOARD_SIZE:
-            print(f"Coordinate numbers must be whole numbers within 0 and {BOARD_SIZE}")
+            print(f"Coordinates must be whole numbers within 0 and {BOARD_SIZE}")
             continue
+
         return (x, y)
     
 def simple_game_loop():
@@ -30,38 +43,52 @@ def simple_game_loop():
     
     print("Welcome to CLI Reversi! :)")
 
+    # Initialise the board and some variables used throughout the whole game
     board = components.initialise_board(BOARD_SIZE)
-
     move_counter = 60
     current_player_colour = "Dark "
     game_won = False
-    components.print_board(board)
     previous_turn_passed = False
 
+    # Print the initial board so the player can see what moves are available easily
+    components.print_board(board)
+
     while not game_won:
+        # Check there is a legal move for the current player
         has_legal_move = False
         for y, row in enumerate(board):
-            for x, space in enumerate(row):
-                if space != "None ":
+            for x, cell in enumerate(row):
+                if cell != "None ":
                     continue
+                
+                # If there is at least one legal move to make then they must continue with their turn
                 if components.legal_move(current_player_colour, (x+1, y+1), board):
                     has_legal_move = True
 
+        # Change players if there are no legal moves and display this to the player to let them know their turn is passed
         if not has_legal_move:
+            # Toggles player
             current_player_colour = "Dark " if current_player_colour == "Light" else "Light"
+
             print(f"{current_player_colour.strip()} has no legal moves! Passing turn")
+
+            # If the previous turn was also passed that means neither player can make a legal move so the game is over
             if previous_turn_passed:
                 print("No more legal moves can be made by either player. Game over!")
                 game_won = True
+
             previous_turn_passed = True
             continue
 
+        # Reset this to False because there is a move that can be made
         previous_turn_passed = False
 
         print(f"{move_counter} max moves left")
         print(f"Its {current_player_colour.strip()}'s turn")
 
         move = (0, 0)
+
+        # Requests a move from the player and checks if it is legal. The player has to enter a legal move for the turn to progress
         while True:
             move = cli_input_coords()
             if components.legal_move(current_player_colour, move, board):
@@ -70,60 +97,79 @@ def simple_game_loop():
 
         move_counter -= 1
 
+        # Convert move position to list indices
         x = move[0] - 1
         y = move[1] - 1
 
+        # Set the cell selected by the player to a counter of their colour
         board[y][x] = current_player_colour
-        opposite_colour = "Dark " if current_player_colour == "Light" else "Light"
+
+        # Stores a set of directions vectors which will act as displacements from the position of the newly placed counter to check which surrounding counters to switch from the opponent's colour to the current player's colour when they are outflanked
         valid_directions = [(-1,-1),(0,-1),(1,-1),(-1,0),(1,0),(-1,1),(0,1),(1,1)]
         directions_to_remove = []
-        spaces_to_flip = []
+        cells_to_flip = []
 
         for step in range(1,BOARD_SIZE):
+            # Remove all direction vectors that were confirmed to not lead to valid outflanked cells in the last iteration
             for d in directions_to_remove:
                 valid_directions.remove(d)
 
-            if len(valid_directions) == 0:
-                break
-
+            # Reset the list so that directions are not removed twice
             directions_to_remove.clear()
 
             for direction in valid_directions:
+                # Get the change in position from the newly placed counter
                 dx = direction[0]*step
                 dy = direction[1]*step
 
+                # Do not check the cell if the position to be searched is outside the board
                 if 0 > x+dx or x+dx >= BOARD_SIZE or 0 > y+dy or y+dy >= BOARD_SIZE: 
                     continue
 
-                check_space = board[y + dy][x + dx]
+                # Get the status of cell being checked
+                check_cell = board[y + dy][x + dx]
 
-                if check_space == "None ":
+                # An empty cell reached before reaching another counter of the same colour means that there are no coutners outflanked in this direction
+                if check_cell == "None ":
                     directions_to_remove.append(direction)
                     
-                elif check_space == current_player_colour:
+                # Checks if a counter of the same colour is reached
+                elif check_cell == current_player_colour:
+                    # Adds the position of the cells in a line between the newly added counter and the discovered counter of the same colour (if any) to a list of positions of counters that have been outflanked
                     for i in range(1, step):
-                        spaces_to_flip.append((x+direction[0]*i,y+direction[1]*i))
+                        cells_to_flip.append((x+direction[0]*i,y+direction[1]*i))
                     directions_to_remove.append(direction)
 
-        
-        for space_coords in spaces_to_flip:
-            x = space_coords[0]
-            y = space_coords[1]
+        # Switch the colour of the counters in the cells that were outflanked by the newly added counter
+        for cell_coords in cells_to_flip:
+            x = cell_coords[0]
+            y = cell_coords[1]
+            
+            # Toggle the colour of the counter at that position
             board[y][x] = "Dark " if board[y][x] == "Light" else "Light"
 
+        # Print the status of the board after the turn for the next player
         components.print_board(board)
+
+        # Change the turn to the other player
         current_player_colour = "Dark " if current_player_colour == "Light" else "Light"
 
+        # If the move counter is depleted then end the game
         if move_counter == 0:
             print("No more moves left. Game over!")
             game_won = True
 
+    # Count final number of counters of each colour on the board
     dark_counters = 0
     light_counters = 0
+
+    # Add up the total counters in every row for each colour 
     for row in board:
         dark_counters += row.count("Dark ")
         light_counters += row.count("Light")
 
+    # Decides who wins the game based on who has a higher number of total counters on the board
+    # If both players have the same amount of counters then the game ends in a draw
     if dark_counters > light_counters:
         print(f"Dark wins the game with {dark_counters} total counters! (Light had {light_counters} counters)")
     elif light_counters > dark_counters:
